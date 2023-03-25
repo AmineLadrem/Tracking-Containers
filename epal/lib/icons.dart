cf349ecd96389767a23a4c820893820221a9a9bd
AmineLadrem <102830544+AmineLadrem@users.noreply.github.com>
2023-03-25 05:43:20 +0100

    add bottom navigation bar

---
+++ epal/lib/icons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class MyFlutterApp {
+  MyFlutterApp._();
+
+  static const _kFontFam = 'MyFlutterApp';
+  static const String? _kFontPkg = null;
+
+  static const IconData container =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
--- epal/lib/pages/admin_home.dart
+++ epal/lib/pages/admin_home.dart
@@ -1,36 +1,80 @@
+import 'package:epal/icons.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class AdminHome extends StatelessWidget {
+class AdminHome extends StatefulWidget {
   static const String routeName = '/adminhome';
   final user = FirebaseAuth.instance.currentUser;
 
   @override
+  _AdminHomeState createState() => _AdminHomeState();
+}
+
+class _AdminHomeState extends State<AdminHome> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
-      backgroundColor: Color(0xFFE1E2E2),
+      backgroundColor: Color(0xFFEAF6F6),
       body: Center(
         child: SingleChildScrollView(
             child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Card(
-            color: Color(0xFFEAF6F6),
-            child: Column(children: <Widget>[
-              SizedBox(
-                height: 10.0,
-                width: 350,
-              ),
-              Image.asset("assets/epal.png", width: 184, height: 140),
-              Image.asset("assets/navire.png", width: 321, height: 397),
-              ElevatedButton(
-                  onPressed: () {
-                    FirebaseAuth.instance.signOut();
-                  },
-                  child: Text('Sign out'))
-            ]),
-          ),
+          child: Column(children: <Widget>[
+            SizedBox(
+              height: 10.0,
+              width: 350,
+            ),
+            Image.asset("assets/epal.png", width: 184, height: 140),
+            Image.asset("assets/navire.png", width: 321, height: 397),
+            ElevatedButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                child: Text('Sign out'))
+          ]),
         )),
+      ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Acceuil',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Rechercher',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(MyFlutterApp.container),
+              label: 'Rechercher',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.local_shipping),
+              label: 'Container',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.black,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
