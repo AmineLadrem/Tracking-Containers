f8c7359d75d5ef03477d21785c685a5b9d96913e
lydiaBn <94543559+lydiaBn@users.noreply.github.com>
2023-03-26 18:18:43 +0100

    Acceuil admin updated

--- epal/lib/pages/admin_home.dart
+++ epal/lib/pages/admin_home.dart
@@ -1,3 +1,6 @@
+import 'package:epal/Pages/GestionConteneurs.dart';
+import 'package:epal/Pages/GestionEmployee.dart';
+import 'package:epal/Pages/GestionModules.dart';
 import 'package:epal/icons.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -33,12 +36,94 @@ class _AdminHomeState extends State<AdminHome> {
               width: 350,
             ),
             Image.asset("assets/epal.png", width: 184, height: 140),
-            Image.asset("assets/navire.png", width: 321, height: 397),
+            Image.asset("assets/navire.png", width: 400, height: 300),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: 260, // Set the desired width here
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                      foregroundColor: MaterialStateProperty.all(Colors.black),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => GestionConteneurs()),
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(MyFlutterApp.container),
+                        Text('Gestion Des Conteneurs'),
+                        SizedBox(width: 7),
+                        Icon(MyFlutterApp.container),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 260, // Set the desired width here
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                      foregroundColor: MaterialStateProperty.all(Colors.black),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => GestionEmployee()),
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(Icons.people_outline),
+                        SizedBox(width: 7),
+                        Text('Gestion Des Employés'),
+                        SizedBox(width: 7),
+                        Icon(Icons.people_outline),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 260, // Set the desired width here
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                      foregroundColor: MaterialStateProperty.all(Colors.black),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => GestionModules()),
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(Icons.place_outlined),
+                        Text('Gestion Des Modules'),
+                        SizedBox(width: 7),
+                        Icon(Icons.place_outlined),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
             ElevatedButton(
                 onPressed: () {
                   FirebaseAuth.instance.signOut();
                 },
-                child: Text('Sign out'))
+                child: Text('Se déconnecter'))
           ]),
         )),
       ),
@@ -59,11 +144,11 @@ class _AdminHomeState extends State<AdminHome> {
             ),
             BottomNavigationBarItem(
               icon: Icon(MyFlutterApp.container),
-              label: 'Rechercher',
+              label: 'Conteneurs',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.local_shipping),
-              label: 'Container',
+              icon: Icon(Icons.people),
+              label: 'employés',
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.person),
