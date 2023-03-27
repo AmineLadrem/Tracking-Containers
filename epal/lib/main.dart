405b7fe1842ffd57daf1dff9da75455ef9afa426
AmineLadrem <102830544+AmineLadrem@users.noreply.github.com>
2023-03-27 23:06:09 +0100

    update main.dart

--- epal/lib/main.dart
+++ epal/lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: LoginPage(),
       routes: Routes.routes,
     );
   }
