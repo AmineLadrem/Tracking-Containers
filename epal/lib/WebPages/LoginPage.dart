873accecd87f83c22e928452ed9d2a5d7b341a67
AmineLadrem <abdulhaqamine.ladrem@etu.usthb.dz>
2023-04-09 17:44:03 +0100

    web app

---
+++ epal/lib/WebPages/LoginPage.dart
@@ -0,0 +1,117 @@
+import 'package:epal/pages/admin_home.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class WebLoginPage extends StatefulWidget {
+  static const String routeName = '/login';
+  @override
+  State<WebLoginPage> createState() => _WebLoginPageState();
+}
+
+class _WebLoginPageState extends State<WebLoginPage> {
+  final emailController = TextEditingController();
+  final PasswordController = TextEditingController();
+
+  Future<void> signIn() async {
+    try {
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: PasswordController.text.trim(),
+      );
+      // If sign-in was successful, navigate to the home page
+      Navigator.pushNamed(context, AdminHome.routeName);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    emailController.dispose();
+    PasswordController.dispose();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xFFEAF6F6),
+        body: Center(
+            child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 40.0,
+                width: 450,
+              ),
+              Image.asset("assets/epal.png", width: 184, height: 140),
+              Image.asset("assets/navire.png", width: 321, height: 397),
+              //2 inputs for username and password
+              Container(
+                width: 300,
+                child: TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Identifiant',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                width: 300,
+                child: TextField(
+                  controller: PasswordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Mot de passe',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              //Login button
+              Container(
+                width: 350,
+                child: ElevatedButton.icon(
+                  onPressed: signIn,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                  ),
+                  icon: Icon(Icons.login_rounded, color: Colors.black),
+                  label: Text(
+                    'Se connecter',
+                    style: TextStyle(
+                      shadows: <Shadow>[
+                        Shadow(
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 3.0,
+                          color: Colors.grey,
+                        ),
+                      ],
+                      color: Colors.black,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+}
