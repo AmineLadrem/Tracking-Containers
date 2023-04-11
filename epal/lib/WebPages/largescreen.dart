2fb8e7ab85ac6729361ae9c358cf962dacf20031
AmineLadrem <abdulhaqamine.ladrem@etu.usthb.dz>
2023-04-11 21:41:13 +0100

    Update largescreen.dart

--- epal/lib/WebPages/largescreen.dart
+++ epal/lib/WebPages/largescreen.dart
@@ -1,4 +1,5 @@
 import 'package:epal/constants/style.dart';
+import 'package:epal/icons.dart';
 import 'package:flutter/material.dart';
 
 class LargeScreen extends StatelessWidget {
@@ -20,7 +21,95 @@ class LargeScreen extends StatelessWidget {
                   height: 700,
                   decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF80CFCC),
+                          // elevation
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () {
+                          // handle button press
+                        },
+                        child: Container(
+                          height: 50,
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(MyFlutterApp.container),
+                              SizedBox(width: 4),
+                              Text('    Conteneurs     ',
+                                  style: TextStyle(
+                                      fontFamily: 'Urbanist',
+                                      fontWeight: FontWeight.bold)),
+                              Icon(MyFlutterApp.container),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF80CFCC),
+                          // elevation
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () {
+                          // handle button press
+                        },
+                        child: Container(
+                          height: 50,
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(Icons.gps_fixed),
+                              SizedBox(width: 4),
+                              Text('Tracker_Module',
+                                  style: TextStyle(
+                                      fontFamily: 'Urbanist',
+                                      fontWeight: FontWeight.bold)),
+                              Icon(Icons.gps_fixed),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 500),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF80CFCC),
+                          // elevation
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () {
+                          // handle button press
+                        },
+                        child: Container(
+                          height: 50,
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(Icons.logout),
+                              SizedBox(width: 4),
+                              Text('Se déconnecter',
+                                  style: TextStyle(
+                                      fontFamily: 'Urbanist',
+                                      fontWeight: FontWeight.bold)),
+                              Icon(Icons.logout),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
