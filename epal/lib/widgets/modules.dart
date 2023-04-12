82c99388ea4be8ade311b8e37013b76fb8d730f0
AmineLadrem <102830544+AmineLadrem@users.noreply.github.com>
2023-04-12 12:22:36 +0100

    update

--- epal/lib/widgets/modules.dart
+++ epal/lib/widgets/modules.dart
@@ -1,11 +1,20 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 
-class modules extends StatelessWidget {
+class modules extends StatefulWidget {
   const modules({super.key});
 
   @override
+  State<modules> createState() => _modulesState();
+}
+
+class _modulesState extends State<modules> {
+  @override
   Widget build(BuildContext context) {
+    GoogleMapController? mapController;
+    Set<Marker> markers = Set(); //markers for google map
+    LatLng showLocation = LatLng(27.7089427, 85.3086209);
+    //location to show in map //contrller for Google ma
     return Container(
       height: 1000,
       width: 300,
@@ -16,7 +25,23 @@ class modules extends StatelessWidget {
           width: 1,
         ),
       ),
-      child: Text("modules"),
+      child: GoogleMap(
+        //Map widget from google_maps_flutter package
+        zoomGesturesEnabled: true, //enable Zoom in, out on map
+        initialCameraPosition: CameraPosition(
+          //innital position in map
+          target: showLocation, //initial position
+          zoom: 10.0, //initial zoom level
+        ),
+        markers: markers, //markers to show on map
+        mapType: MapType.normal, //map type
+        onMapCreated: (controller) {
+          //method called when map is created
+          setState(() {
+            mapController = controller;
+          });
+        },
+      ),
     );
   }
 }
