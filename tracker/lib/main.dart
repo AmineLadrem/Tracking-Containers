95cc493afbea0ff2b5692df90dc56d3c819ba464
AmineLadrem <abdulhaqamine.ladrem@etu.usthb.dz>
2023-04-25 06:13:39 +0100

    background

--- tracker/lib/main.dart
+++ tracker/lib/main.dart
@@ -1,39 +1,69 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:tracker/tracker.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:workmanager/workmanager.dart';
 
-const taskManager = 'First Task';
-void callbackDispatcher() {
-  Workmanager().executeTask((taskManager, inputData) {
-    switch (taskManager) {
-      case 'First Task':
-        print('First Task');
-        break;
-    }
-    return Future.value(true);
-  });
-}
-
-Future<void> main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  Workmanager().initialize(
-      callbackDispatcher, // The top level function, aka callbackDispatcher
-      isInDebugMode:
-          true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
-      );
-  Workmanager().registerOneOffTask("task-identifier", "simpleTask");
+  // Start the background service
+  FlutterBackgroundService().startService();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+void onStart() {
+  // Start the timer when the background service is started
+  Timer.periodic(Duration(seconds: 2), (timer) {
+    print('Hello from background service!');
+    // Update the service status
+    FlutterBackgroundService().on('onStart').listen((event) {
+      // Here is the current status
+      // You can use it to update your UI or send it to the server
+      print(event);
+    });
+  });
+}
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Tracker(),
+      title: 'Background Service Demo',
+      home: BackgroundService(),
     );
+  }
+}
+
+class BackgroundService extends StatefulWidget {
+  @override
+  _BackgroundServiceState createState() => _BackgroundServiceState();
+}
+
+class _BackgroundServiceState extends State<BackgroundService> {
+  int _count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    // Start the background service
+    FlutterBackgroundService().startService();
+    // Listen for service updates
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Background Service Demo'),
+      ),
+      body: Center(
+        child: Text('Count: $_count'),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    // Stop the background service when the widget is disposed
+
+    super.dispose();
   }
 }
