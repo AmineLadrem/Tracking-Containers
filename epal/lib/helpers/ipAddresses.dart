0ac8cd68811feeecbcc12ebca4f2d682eb083064
AmineLadrem <102830544+AmineLadrem@users.noreply.github.com>
2023-05-07 15:53:29 +0100

    update

--- epal/lib/helpers/ipAddresses.dart
+++ epal/lib/helpers/ipAddresses.dart
@@ -15,3 +15,4 @@ final String usedIPAddress = 'http://192.168.0.7:8000';
 //final String usedIPAddress = LydiaIPAddresses[0]; // works with real device
 //final String usedIPAddress = LydiaIPAddresses[1]; // works with real device
 //final String usedIPAddress = ipAddresses[3]; //works with emulator
+
