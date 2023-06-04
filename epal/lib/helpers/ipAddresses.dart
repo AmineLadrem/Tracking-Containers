ac14632df857741f72610c8865eea0b300563621
AmineLadrem <abdulhaqamine.ladrem@etu.usthb.dz>
2023-06-04 09:32:05 +0100

    Update ipAddresses.dart

--- epal/lib/helpers/ipAddresses.dart
+++ epal/lib/helpers/ipAddresses.dart
@@ -3,11 +3,12 @@ final List<String> ipAddresses = [
   'http://192.168.1.101:8000',
   'http://192.168.248.16:8000',
   'http://192.168.184.189:8000',
+  'https://a6ad-105-102-55-161.ngrok-free.app'
 
   // add more IP addresses here as needed
 ];
 
-final String usedIPAddress = ipAddresses[1];
+final String usedIPAddress = ipAddresses[4];
 
 final List<String> LydiaIPAddresses = [
   'http://197.204.60.137:8000',
