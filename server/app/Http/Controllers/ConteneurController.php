d6feb2d0a2c2d784b98a4e61de4fd1cb4fa610b1
AmineLadrem <102830544+AmineLadrem@users.noreply.github.com>
2023-06-06 12:27:07 +0100

    Update ConteneurController.php

--- server/app/Http/Controllers/ConteneurController.php
+++ server/app/Http/Controllers/ConteneurController.php
@@ -139,8 +139,10 @@ class ConteneurController extends Controller
     {
         print($cont_id);
         print($parc);
+        $cont=Conteneur::where('Cont_ID', $cont_id)->first();
+        return response()->json($cont);
     if($parc==1  &&$parc==2 ){
-       print( Conteneur::where('Cont_ID', $cont_id)->first());
+       return response()->json($cont);
        Conteneur::where('Cont_ID', $cont_id)->update(['Cont_Status'=>'débarqué','NumParc'=>$parc]);
     }
     elseif($parc>=3 && $parc<=8){
