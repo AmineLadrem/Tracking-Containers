315c21f024692971964457aff44bab5a40797cff
AmineLadrem <abdulhaqamine.ladrem@etu.usthb.dz>
2023-06-18 11:55:48 +0100

    update IP address

--- epal/lib/WebPages/ipAddress.dart
+++ epal/lib/WebPages/ipAddress.dart
@@ -1,6 +1,6 @@
 final List<String> ipAddresses = [
   'http://127.0.0.1:8000',
-  'https://76cb-197-204-210-65.ngrok-free.app'
+  'https://20c3-105-100-39-91.ngrok-free.app'
 
   // add more IP addresses here as needed
 ];
